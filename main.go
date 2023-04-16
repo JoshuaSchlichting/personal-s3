64b096f51ffe9d7db60a6fc02f01336b15391081
Joshua Schlichting <7680545+JoshuaSchlichting@users.noreply.github.com>
2023-04-16 18:53:09 -0400

    rearranging code

--- main.go
+++ main.go
@@ -29,6 +29,96 @@ func newS3ClientWrapper() (*s3ClientWrapper, error) {
 	}, nil
 }
 
+func (c *s3ClientWrapper) SyncFolderToBucket(bucketName string, dir string) error {
+
+	// Get list of files in folder
+	files, err := ListDir(dir)
+	if err != nil {
+		return fmt.Errorf("failed to list files in folder: %v", err)
+	}
+
+	relativeFileNames := make([]string, len(files))
+	// get filenames relative to the target directory
+	for i, file := range files {
+		relativeFileNames[i] = file[len(dir)-len(filepath.Base(dir)):]
+	}
+
+	// Upload files to S3 bucket
+	for j, file := range relativeFileNames {
+		exists, err := c.ObjectExists(bucketName, file)
+		if err != nil {
+			log.Printf("failed to check if object exists: %v", err)
+			return err
+		}
+		if exists {
+			log.Printf("object %s already exists in bucket %s, skipping", file, bucketName)
+			continue
+		}
+		log.Print("uploading file ", file, " to bucket", bucketName)
+		err = UploadFileToBucket(c.s3Client, bucketName, files[j])
+		log.Print("uploaded complete:", file)
+		if err != nil {
+			log.Printf("failed to upload file %s: %v", file, err)
+		}
+	}
+
+	return nil
+}
+
+func (c *s3ClientWrapper) ObjectExists(bucketName string, key string) (bool, error) {
+	_, err := c.s3Client.HeadObject(context.TODO(), &s3.HeadObjectInput{
+		Bucket: &bucketName,
+		Key:    &key,
+	})
+	if err != nil {
+		if strings.Contains(err.Error(), "NotFound") {
+			return false, nil
+		}
+		return false, fmt.Errorf("failed to check if object exists: %v", err)
+	}
+
+	return true, nil
+}
+
+func ListDir(dir string) ([]string, error) {
+	var files []string
+
+	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() {
+			files = append(files, path)
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to walk folder: %v", err)
+	}
+
+	return files, nil
+}
+
+func UploadFileToBucket(s3Client *s3.Client, bucketName string, filePath string) error {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return fmt.Errorf("failed to open file %s: %v",
+			filePath, err)
+	}
+	defer file.Close()
+
+	_, err = s3Client.PutObject(context.TODO(), &s3.PutObjectInput{
+		Bucket: &bucketName,
+		Key:    &filePath,
+		Body:   file,
+	})
+	if err != nil {
+		return fmt.Errorf("failed to put object: %v", err)
+	}
+
+	return nil
+}
+
 func main() {
 	// Parse command line flags
 	bucketNamePtr := flag.String("bucket", "", "name of the S3 bucket")
@@ -53,95 +143,4 @@ func main() {
 	}
 
 	fmt.Println("Sync complete!")
-}
-
-func (c *s3ClientWrapper) SyncFolderToBucket(bucketName string, dir string) error {
-
-	// Get list of files in folder
-	files, err := ListDir(dir)
-	if err != nil {
-		return fmt.Errorf("failed to list files in folder: %v", err)
-	}
-
-	relativeFileNames := make([]string, len(files))
-	// get filenames relative to the target directory
-	for i, file := range files {
-		relativeFileNames[i] = file[len(dir)-len(filepath.Base(dir)):]
-	}
-
-	// Upload files to S3 bucket
-	for j, file := range relativeFileNames {
-		// Check if file already exists in bucket
-		exists, err := c.ObjectExists(bucketName, file)
-		if err != nil {
-			log.Printf("failed to check if object exists: %v", err)
-			return err
-		}
-		if exists {
-			log.Printf("object %s already exists in bucket %s, skipping", file, bucketName)
-			continue
-		}
-		log.Print("uploading file ", file, " to bucket", bucketName)
-		err = UploadFileToBucket(c.s3Client, bucketName, files[j])
-		log.Print("uploaded complete:", file)
-		if err != nil {
-			log.Printf("failed to upload file %s: %v", file, err)
-		}
-	}
-
-	return nil
-}
-
-func ListDir(dir string) ([]string, error) {
-	var files []string
-
-	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-		if !info.IsDir() {
-			files = append(files, path)
-		}
-		return nil
-	})
-	if err != nil {
-		return nil, fmt.Errorf("failed to walk folder: %v", err)
-	}
-
-	return files, nil
-}
-
-func (c *s3ClientWrapper) ObjectExists(bucketName string, key string) (bool, error) {
-	_, err := c.s3Client.HeadObject(context.TODO(), &s3.HeadObjectInput{
-		Bucket: &bucketName,
-		Key:    &key,
-	})
-	if err != nil {
-		if strings.Contains(err.Error(), "NotFound") {
-			return false, nil
-		}
-		return false, fmt.Errorf("failed to check if object exists: %v", err)
-	}
-
-	return true, nil
-}
-
-func UploadFileToBucket(s3Client *s3.Client, bucketName string, filePath string) error {
-	file, err := os.Open(filePath)
-	if err != nil {
-		return fmt.Errorf("failed to open file %s: %v",
-			filePath, err)
-	}
-	defer file.Close()
-
-	_, err = s3Client.PutObject(context.TODO(), &s3.PutObjectInput{
-		Bucket: &bucketName,
-		Key:    &filePath,
-		Body:   file,
-	})
-	if err != nil {
-		return fmt.Errorf("failed to put object: %v", err)
-	}
-
-	return nil
 }
